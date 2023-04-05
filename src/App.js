a1a45fd99d3ce2bcc5e1b7bedf94cd4b262e4e4f
Feres <feres.cherif150817@gmail.com>
2023-04-05 14:43:10 +0100

    Budget calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import Alert from './components/Alert';
+import { v4 as uuidv4 } from 'uuid';
+
+const initialExpenses = localStorage.getItem('expenses') ? JSON.parse(localStorage.getItem('expenses')) : [];
 
 function App() {
+
+  // ********* state values ***********
+  // all expenses , add expense
+  const [expenses, setExpenses] = useState(initialExpenses);
+  // single expense 
+  const [charge, setCharge] = useState('');
+  // single amount
+  const [amount, setAmount] = useState('');
+  // alert
+  const [alert, setAlert] = useState({ show: false });
+  // Edit
+  const [edit, setEdit] = useState(false);
+  // Edit item
+  const [id, setId] = useState(0);
+
+  // ********* useEffect ***********
+  useEffect(() => {
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+  }, [expenses]);
+
+  // ********* functionality ***********
+  //  handle charge
+  const handleCharge = e => {
+    setCharge(e.target.value)
+  }
+  //  handle amount
+  const handleAmount = e => {
+    setAmount(e.target.value)
+  };
+  //  handle alert
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false })
+    }, 3000)
+  }
+  //  handle submit
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (charge !== '' && amount > 0) {
+      if (edit) {
+        let tempExpenses = expenses.map(item => {
+          return item.id === id ? { ...item, charge, amount } : item
+        });
+        setExpenses(tempExpenses);
+        setEdit(false);
+        handleAlert({ type: 'success', text: "Item edited" });
+      }
+      else {
+        const singleExpense = { id: uuidv4(), charge, amount };
+        setExpenses([...expenses, singleExpense]);
+        handleAlert({ type: 'success', text: "Item added" });
+      }
+      setCharge('');
+      setAmount('');
+    }
+    else {
+      // handle alert called
+      handleAlert({ type: 'danger', text: `Charge can't be empty value and amount value has to be bigger than zero` })
+    }
+  };
+  // clear all items
+  const clearItems = () => {
+    setExpenses([]);
+    handleAlert({ type: 'danger', text: 'all items deleted' })
+
+  }
+  // handle delete
+  const handleDelete = (id) => {
+    let tempExpenses = expenses.filter(item => item.id !== id);
+    setExpenses(tempExpenses);
+    handleAlert({ type: 'danger', text: 'item deleted' })
+  }
+  // handle edit
+  const handleEdit = id => {
+    let expense = expenses.find(item => item.id === id)
+    let { charge, amount } = expense;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
+      <Alert />
+      <h1>Budget Calculator</h1>
+      <main className='App'>
+        <ExpenseForm charge={charge} amount={amount} handleCharge={handleCharge}
+          handleAmount={handleAmount} handleSubmit={handleSubmit}
+          edit={edit}
+        />
+        <ExpenseList expenses={expenses} handleDelete={handleDelete} handleEdit={handleEdit}
+          clearItems={clearItems} />
+      </main>
+      <h1>Total spending :  <span className='total'>
+        ${expenses.reduce((acc, curr) => {
+          return (acc += parseInt(curr.amount))
+        }, 0)}
+      </span>
+      </h1>
+    </>
+
   );
 }
 
